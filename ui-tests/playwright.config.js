19130df97d49f67c1415e939a8f2ef602836b5d2
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-04-12 12:18:17 +0000

    Use Playwright `webServer` option

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -4,5 +4,13 @@ module.exports = {
   use: {
     baseURL: 'http://localhost:8866',
     video: 'retain-on-failure',
+    webServer: [
+      {
+        command: 'yarn start',
+        port: 8866,
+        timeout: 120 * 1000,
+        reuseExistingServer: true,
+      },
+    ],
   },
 };
