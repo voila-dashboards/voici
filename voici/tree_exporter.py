80ea4238829891295699e50b383813cb94b82c37
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-04-12 21:18:26 +0200

    Fix themesUrl

--- voici/tree_exporter.py
+++ voici/tree_exporter.py
@@ -54,11 +54,11 @@ def patch_page_config(page_config: Dict, relative_path: Path):
         "settingsUrl"
     ] = f"../../{'../' * len(relative_path.parts)}build/schemas"
     page_config_copy[
-        "themesUrl"
-    ] = f"../../../{'../' * len(relative_path.parts)}build/themes"
-    page_config_copy[
         "fullLabextensionsUrl"
     ] = f"../../{'../' * len(relative_path.parts)}extensions"
+
+    # The Themes URL will be joined with the base URL
+    page_config_copy["themesUrl"] = "./build/themes"
 
     return page_config_copy
 
