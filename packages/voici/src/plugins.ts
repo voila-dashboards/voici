5bf59cadc627698e3605b34ab56d6debdd033ed2
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-04-13 15:26:20 +0200

    Fix extra network theme requests (#62)
    * Fix extra network requests
    
    * Restore labThemeName page config option for now

--- packages/voici/src/plugins.ts
+++ packages/voici/src/plugins.ts
@@ -50,12 +50,16 @@ export const themePlugin: JupyterFrontEndPlugin<void> = {
     if (!themeManager) {
       return;
     }
+
     const labThemeName = PageConfig.getOption('labThemeName');
 
     const search = window.location.search;
     const urlParams = new URLSearchParams(search);
     const urltheme = urlParams.get('theme');
     let theme = urltheme ? decodeURIComponent(urltheme) : labThemeName;
+
+    // default to the light theme if the theme is not specified (empty)
+    theme = theme || 'light';
 
     if (theme === 'dark') {
       theme = 'JupyterLab Dark';
