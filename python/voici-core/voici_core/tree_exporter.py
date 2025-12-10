729e18f87ee7a23897da0a4170680dbaedff3284
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-12-10 09:17:19 -0500

    emulate upstream config-utils relative path rewriting (#138)
    * emulate upstream config-utils relative path rewriting
    
    * blacken
    
    * patch relative urls first, then overload with voici-specific paths
    
    * update comments
    
    * fix paths for settings/extensions
    
    ---------
    
    Co-authored-by: Jeremy Tuloup <jeremy.tuloup@gmail.com>

--- python/voici-core/voici_core/tree_exporter.py
+++ python/voici-core/voici_core/tree_exporter.py
@@ -2,7 +2,7 @@ from copy import deepcopy
 from functools import partial
 from io import StringIO
 from pathlib import Path
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Any
 
 import jinja2
 from jupyter_server.utils import url_escape, url_path_join
@@ -39,22 +39,30 @@ def path_to_content(path: Path, relative_to: Path):
     return None
 
 
-def patch_page_config(page_config: Dict, relative_path: Path, config: VoilaConfiguration):
+def patch_page_config(
+    page_config: Dict, relative_path: Path, config: VoilaConfiguration
+):
+    """Update page config JSON to use ``voici` paths."""
     page_config_copy = deepcopy(page_config)
 
-    # Align the base url with the relative path
-    page_config_copy['baseUrl'] = '../../' + '../' * len(relative_path.parts)
+    voici_prefix = '../' * (1 + len(relative_path.parts))
+    base_prefix = f'../{voici_prefix}'
 
-    # Grabbing from the Voici static folder
-    page_config_copy['fullStaticUrl'] = f'../{"../" * len(relative_path.parts)}build'
+    # emulate upstream ``config-utils.js``
+    patch_relative_urls(page_config_copy, base_prefix)
 
-    # Grabbing from the jupyterlite static folders
-    page_config_copy['settingsUrl'] = f'../../{"../" * len(relative_path.parts)}build/schemas'
-    page_config_copy['fullLabextensionsUrl'] = f'../../{"../" * len(relative_path.parts)}extensions'
-
-    # The Themes URL will be joined with the base URL in the
-    # JupyterLite main application
-    page_config_copy['themesUrl'] = './build/themes'
+    page_config_copy.update(
+        # Align the base URL with the relative path
+        baseUrl=base_prefix,
+        # Grabbing from the Voici static folder
+        fullStaticUrl=f'{voici_prefix}build',
+        # Grabbing from the jupyterlite static folders
+        settingsUrl=f'{base_prefix}build/schemas',
+        fullLabextensionsUrl=f'{base_prefix}extensions',
+        # The Themes URL will be joined with the base URL in the
+        # JupyterLite main application
+        themesUrl='./build/themes',
+    )
 
     if config.theme == 'light':
         themeName = 'JupyterLab Light'
@@ -75,6 +83,26 @@ def patch_page_config(page_config: Dict, relative_path: Path, config: VoilaConfi
         disabled_extensions=disabled_extensions,
     )
     return page_config_copy
+
+
+def patch_relative_urls(config_dict: Dict[str, Any], path_prefix: str) -> None:
+    """Update one ``page_config`` object in place with relative URLs.
+
+    See: https://github.com/jupyterlite/jupyterlite/blame/v0.7.0b1/app/config-utils.js
+    """
+    for key, value in config_dict.items():
+        if key in ['licensesUrl', 'themesUrl', 'federated_extensions']:
+            # these are left unchanged, as they are handled in upstream JS code
+            continue
+        elif isinstance(value, dict):
+            # nested config objects may also contain relative paths
+            patch_relative_urls(value, path_prefix)
+        elif key.endswith('Url') and isinstance(value, str) and value.startswith('./'):
+            config_dict[key] = f'{path_prefix}{value[2:]}'
+        elif key.endswith('Urls') and isinstance(value, list):
+            config_dict[key] = [
+                f'{path_prefix}{v[2:]}' if v.startswith('./') else v for v in value
+            ]
 
 
 class VoiciTreeExporter(HTMLExporter):
